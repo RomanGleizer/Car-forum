222b31bed421f9bb439c6ecb4fe5cbe0993dbed6
Roman <125725530+RomanGleizer@users.noreply.github.com>
2024-01-10 11:58:49 +0500

    First Commit

---
+++ CarForum/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using CarForum.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CarForum.Controllers;
+
+[Route("api/{controller}")]
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("Index")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
